627e58b38dfef22e3b44e1596650ccf5570cd36d
Valério Hasman <valeriopro22@gmail.com>
2022-08-12 11:40:55 -0300

    Base e tela

--- Views/home/home.php
+++ Views/home/home.php
@@ -1,25 +1,41 @@
 <button type="button">Gravar</button>
 <button type="button">Ler</button>
+<button type="button">Remover</button>
+<button type="button">Remover filho</button>
+<button type="button">Adicionar filho</button>
 <br />
 <br />
 <label for="nome">Nome</label>
 <input type="text" name="pessoa" id="nome">
 <button type="button">Incluir</button>
-
-<table>
-  <thead>
-    <tr>
-      <th colspan="2" scope="col">Pessas</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Mark</td>
-      <td>Otto</td>
-    </tr>
-    <tr>
-      <td>Jacob</td>
-      <td>Thornton</td>
-    </tr>
-  </tbody>
-</table>
+<br />
+<br />
+<div class="dados">
+  <table>
+    <thead>
+      <tr>
+        <th colspan="2" scope="col">Pessas</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="pessoa">
+        <td>Marcos</td>
+        <td><button type="button">Remover</button></td>
+      </tr>
+      <tr class="filho">
+        <td class="nome">Pedro</td>
+        <td><button type="button">Remover filho</button></td>
+      </tr>
+      <tr class="filho">
+        <td class="nome">Pedro</td>
+        <td><button type="button">Remover filho</button></td>
+      </tr>
+      <tr class="adicionarFilho">
+        <td colspan="2" scope="col"><button type="button">Adicionar filho</button></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<div class="dados">
+  <textarea id="json" cols="30" rows="10"></textarea>
+</div>
